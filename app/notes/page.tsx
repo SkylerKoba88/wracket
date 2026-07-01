e2409e95311e246315bdb7e40c69da50ece21be5
SkylerKoba88 <sfk5879@psu.edu>
2026-07-01 15:05:28 -0400

    deleted notes

--- app/notes/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getInventoryItems } from '@/lib/supabase/products';
-
-export default async function Notes() {
-  const notes = await getInventoryItems();
-
-  return <pre>{JSON.stringify(notes, null, 2)}</pre>
-}
