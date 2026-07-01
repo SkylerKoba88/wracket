5799dd29674db64f0eeaa6e35c700eea8509c03c
SkylerKoba88 <sfk5879@psu.edu>
2026-07-01 15:02:28 -0400

    fixing shop fetch

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,12 +1,17 @@
 import { Storefront } from "@/components/storefront";
 import { getInventoryItems } from "@/lib/supabase/products";
+import {Suspense} from "react";
 
 export default async function ShopPage() {
   console.log("Shoppage env", {
-    NEXT_PUBLIC_SUPABASE_URL: !!process.env.NEXT_PUBLIC_STORAGE_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY: !!process.env.NEXT_PUBLIC_STORAGE_SUPABASE_PUBLISHABLE_KEY,
+    NEXT_PUBLIC_STORAGE_SUPABASE_URL: !!process.env.NEXT_PUBLIC_STORAGE_SUPABASE_URL,
+    NEXT_PUBLIC_STORAGE_SUPABASE_PUBLISHABLE_KEY: !!process.env.NEXT_PUBLIC_STORAGE_SUPABASE_PUBLISHABLE_KEY,
   });
   const items = await getInventoryItems();
 
-  return <Storefront items={items} />;
+  return (
+    <Suspense fallback="Loading...">
+      <Storefront items={items} />
+    </Suspense>
+  );
 }
